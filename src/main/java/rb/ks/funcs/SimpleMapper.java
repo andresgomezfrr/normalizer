7dfb79911e5b31766f93bb3d913273ea0f6b8d5e
Andres Gomez Ferrer <andresgomezfrr@gmail.com>
2016-09-12 10:17:34 +0200

    Integrate funcs into StreamBuilder. Refactor MapperModel to MapperFunction.

---
+++ src/main/java/rb/ks/funcs/SimpleMapper.java
@@ -0,0 +1,93 @@
+package rb.ks.funcs;
+
+import org.apache.kafka.streams.KeyValue;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class SimpleMapper extends MapperFunction {
+    List<MapperModel> mappers = new ArrayList<>();
+
+    @Override
+    public void prepare(Map<String, Object> properties) {
+        List<Map<String, Object>> maps = (List<Map<String, Object>>) properties.get("maps");
+        maps.forEach(map -> mappers.add(new MapperModel((List<String>) map.get("dimPath"), (String) map.get("as"))));
+    }
+
+    @Override
+    public KeyValue<String, Map<String, Object>> process(String key, Map<String, Object> value) {
+        Map<String, Object> newEvent = new HashMap<>();
+
+        for (MapperModel mapper : mappers) {
+            Integer depth = mapper.dimPath.size() - 1;
+
+            Map<String, Object> levelPath = new HashMap<>(value);
+            for (Integer level = 0; level < depth; level++) {
+                if (levelPath != null) {
+                    levelPath = (Map<String, Object>) levelPath.get(mapper.dimPath.get(level));
+                }
+            }
+
+            if (levelPath != null) newEvent.put(mapper.as, levelPath.get(mapper.dimPath.get(depth)));
+        }
+
+        return new KeyValue<>(key, newEvent);
+    }
+
+    @Override
+    public void stop() {
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("[");
+        mappers.forEach(mapper ->
+                builder.append("{")
+                        .append("dimPath: ").append(mapper.dimPath).append(", ")
+                        .append("as: ").append(mapper.as)
+                        .append("}")
+        );
+        builder.append("]");
+
+        return builder.toString();
+    }
+
+
+    private class MapperModel {
+        List<String> dimPath;
+        String as;
+
+        MapperModel(List<String> dimPath,
+                           String as) {
+            this.dimPath = dimPath;
+
+            if (as != null ) {
+                this.as = as;
+            } else if(dimPath != null) {
+                this.as = dimPath.get(dimPath.size() - 1);
+            }
+        }
+
+        public List<String> getDimPath() {
+            return dimPath;
+        }
+
+        public String getAs() {
+            return as;
+        }
+
+        @Override
+        public String toString() {
+            StringBuilder builder = new StringBuilder();
+            builder.append("{")
+                    .append("dimPath: ").append(dimPath).append(", ")
+                    .append("as: ").append(as)
+                    .append("}");
+
+            return builder.toString();
+        }
+    }
+}
