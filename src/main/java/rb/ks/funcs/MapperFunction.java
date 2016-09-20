9825d6eb0567e810f44c5dba0b39f8c65cda8b1b
Andres Gomez Ferrer <andresgomezfrr@gmail.com>
2016-09-20 11:33:38 +0200

    Add logs

--- src/main/java/rb/ks/funcs/MapperFunction.java
+++ src/main/java/rb/ks/funcs/MapperFunction.java
@@ -2,16 +2,20 @@ package rb.ks.funcs;
 
 import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.kstream.KeyValueMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Map;
 
 
 public abstract class MapperFunction implements Function<KeyValue<String, Map<String, Object>>>,
         KeyValueMapper<String, Map<String, Object>, KeyValue<String, Map<String, Object>>> {
+    private final Logger log = LoggerFactory.getLogger(getClass());
 
     @Override
     public void init(Map<String, Object> properties) {
         prepare(properties);
+        log.info("   with {}", toString());
     }
 
     @Override
