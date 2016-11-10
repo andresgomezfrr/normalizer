1fc2f1485ea55ed6f83084abc7716ace96eea3e8
Andrés Gómez <andresgomezfrr@gmail.com>
2016-11-10 08:56:29 +0100

    Add AndFilter and OrFilter (#6)
    * Add AndFilter and OrFilter
    
    * Fix match conditions on AndFilter and OrFilter

---
+++ service/src/test/java/zz/ks/funcs/impl/OrFilterUnitTest.java
@@ -0,0 +1,127 @@
+package zz.ks.funcs.impl;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.kafka.streams.StreamsConfig;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import zz.ks.builder.StreamBuilder;
+import zz.ks.builder.config.Config;
+import zz.ks.exceptions.PlanBuilderException;
+import zz.ks.funcs.Function;
+import zz.ks.model.PlanModel;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.*;
+
+public class OrFilterUnitTest {
+    static Config config = new Config();
+
+    static {
+        config.put(StreamsConfig.APPLICATION_ID_CONFIG, "app-id-1");
+    }
+
+    private static StreamBuilder streamBuilder = new StreamBuilder(config, null);
+
+    @BeforeClass
+    public static void initTest() throws IOException, PlanBuilderException {
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        File file = new File(classLoader.getResource("or-filter.json").getFile());
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        PlanModel model = objectMapper.readValue(file, PlanModel.class);
+        streamBuilder.builder(model);
+    }
+
+    @Test
+    public void building() {
+        Map<String, Function> functions = streamBuilder.getFunctions("stream1");
+        Function myFunc = functions.get("myFilter");
+
+        assertNotNull(myFunc);
+        assertTrue(myFunc instanceof OrFilter);
+        OrFilter myFilter = (OrFilter) myFunc;
+        assertEquals(2, myFilter.filters.size());
+    }
+
+    @Test
+    public void processGoodSimpleMessage1() {
+        Map<String, Function> functions = streamBuilder.getFunctions("stream1");
+        Function myFunc = functions.get("myFilter");
+
+        assertNotNull(myFunc);
+        assertTrue(myFunc instanceof OrFilter);
+        OrFilter myFilter = (OrFilter) myFunc;
+
+        Map<String, Object> message = new HashMap<>();
+        message.put("timestamp", 123456789L);
+        message.put("FILTER-DIMENSION", "FILTER-VALUE");
+
+        assertTrue(myFilter.process("key1", message));
+    }
+
+    @Test
+    public void processGoodSimpleMessage2() {
+        Map<String, Function> functions = streamBuilder.getFunctions("stream1");
+        Function myFunc = functions.get("myFilter");
+
+        assertNotNull(myFunc);
+        assertTrue(myFunc instanceof OrFilter);
+        OrFilter myFilter = (OrFilter) myFunc;
+
+        Map<String, Object> message = new HashMap<>();
+        message.put("timestamp", 123456789L);
+        message.put("A", "A");
+        message.put("B", "B");
+        message.put("C", "C");
+
+        assertTrue(myFilter.process("key1", message));
+    }
+
+    @Test
+    public void processGoodSimpleMessage3() {
+        Map<String, Function> functions = streamBuilder.getFunctions("stream1");
+        Function myFunc = functions.get("myFilter");
+
+        assertNotNull(myFunc);
+        assertTrue(myFunc instanceof OrFilter);
+        OrFilter myFilter = (OrFilter) myFunc;
+
+        Map<String, Object> message = new HashMap<>();
+        message.put("timestamp", 123456789L);
+        message.put("FILTER-DIMENSION", "FILTER-VALUE");
+        message.put("A", "A");
+        message.put("B", "B");
+        message.put("C", "C");
+
+        assertTrue(myFilter.process("key1", message));
+    }
+
+    @Test
+    public void processGoodSimpleMessage4() {
+        Map<String, Function> functions = streamBuilder.getFunctions("stream1");
+        Function myFunc = functions.get("myFilter");
+
+        assertNotNull(myFunc);
+        assertTrue(myFunc instanceof OrFilter);
+        OrFilter myFilter = (OrFilter) myFunc;
+
+        Map<String, Object> message = new HashMap<>();
+        message.put("timestamp", 123456789L);
+        message.put("X", "Y");
+        message.put("B", "B");
+        message.put("C", "C");
+
+        assertFalse(myFilter.process("key1", message));
+    }
+
+
+    @AfterClass
+    public static void stop(){
+        streamBuilder.close();
+    }
+}
