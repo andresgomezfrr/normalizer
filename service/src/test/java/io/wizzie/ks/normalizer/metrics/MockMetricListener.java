e4683e1385836fa31649af926ecb0da50b973711
Juan Chorro <jjchorrobe@gmail.com>
2016-12-05 08:24:39 +0100

    Fix metric register (#14)
    * Add verbose information and new methods for new metrics
    
    * Add new configuration parameter for metric
    
    * Get 'verboseMode' from configuration in 'MetricsManager' constructor
    
    * Add log4j.xml for log configuration
    
    * Add new unit test for check if data is send to console
    
    * Fix name for some metrics values
    
    * Add mock of 'MetricListener' interface
    
    * Fix and rename methods

---
+++ service/src/test/java/io/wizzie/ks/normalizer/metrics/MockMetricListener.java
@@ -0,0 +1,38 @@
+package io.wizzie.ks.normalizer.metrics;
+
+import io.wizzie.ks.normalizer.builder.config.Config;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MockMetricListener implements MetricListener {
+
+    List<Map<String, Object>> content = new ArrayList<>();
+
+    @Override
+    public void init(Config config) {
+    }
+
+    @Override
+    public void updateMetric(String metricName, Object metricValue) {
+        Map<String, Object> metric = new HashMap<>();
+        metric.put("timestamp", System.currentTimeMillis() / 1000L);
+        metric.put("monitor", metricName);
+        metric.put("value", metricValue);
+
+        if (metricValue != null)
+            content.add(metric);
+    }
+
+    @Override
+    public void close() {
+        //Nothing to do
+    }
+
+    @Override
+    public String name() {
+        return "mock";
+    }
+}
