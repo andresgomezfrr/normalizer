9b62a4d90302f52f69f277911e3a1dd300529e46
Andres Gomez Ferrer <andresgomezfrr@gmail.com>
2017-02-14 11:22:11 +0200

    Change normalizer to use config file instead of args

--- src/main/java/rb/ks/Normalizer.java
+++ src/main/java/rb/ks/Normalizer.java
@@ -1,34 +1,17 @@
 package rb.ks;
 
-import org.apache.kafka.common.serialization.Serdes;
-import org.apache.kafka.streams.StreamsConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import rb.ks.builder.Builder;
-import rb.ks.exceptions.PlanBuilderException;
-import rb.ks.serializers.JsonSerde;
-
-import java.io.File;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
+import rb.ks.builder.config.Config;
 
 public class Normalizer {
     private static final Logger log = LoggerFactory.getLogger(Normalizer.class);
 
-    public static void main(String[] args) throws IOException, PlanBuilderException {
-        if (args.length == 5) {
-            File file = new File(args[0]);
-
-            Map<String, String> configuration = new HashMap<>();
-            configuration.put(StreamsConfig.ZOOKEEPER_CONNECT_CONFIG, args[1]);
-            configuration.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, args[2]);
-            configuration.put(StreamsConfig.APPLICATION_ID_CONFIG, args[3]);
-            configuration.put(StreamsConfig.NUM_STREAM_THREADS_CONFIG, args[4]);
-            configuration.put(StreamsConfig.KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
-            configuration.put(StreamsConfig.VALUE_SERDE_CLASS_CONFIG, JsonSerde.class.getName());
-
-            Builder builder = new Builder(configuration);
+    public static void main(String[] args) throws Exception {
+        if (args.length == 1) {
+            Config config = new Config(args[0]);
+            Builder builder = new Builder(config);
 
             Runtime.getRuntime().addShutdownHook(new Thread(() -> {
                 builder.close();
@@ -37,7 +20,7 @@ public class Normalizer {
 
 
         } else {
-            log.error("Execute: java -cp ${JAR_PATH} rb.ks.Normalizer <config_file> <zookeeper_connect> <bootstrap_servers> <application_id> <threads>");
+            log.error("Execute: java -cp ${JAR_PATH} rb.ks.Normalizer <config_file>");
         }
     }
 }
