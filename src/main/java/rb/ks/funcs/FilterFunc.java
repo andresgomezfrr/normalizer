6ebae7a9ae9ee21ea769beec2a65f3f9a909d5b2
Andres Gomez Ferrer <andresgomezfrr@gmail.com>
2017-02-14 11:22:11 +0200

    Add logs

--- src/main/java/rb/ks/funcs/FilterFunc.java
+++ src/main/java/rb/ks/funcs/FilterFunc.java
@@ -1,13 +1,18 @@
 package rb.ks.funcs;
 
 import org.apache.kafka.streams.kstream.Predicate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Map;
 
 public abstract class FilterFunc implements Function<Boolean>, Predicate<String, Map<String, Object>> {
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
     @Override
     public void init(Map<String, Object> properties) {
         prepare(properties);
+        log.info("   with {}", toString());
     }
 
     @Override
