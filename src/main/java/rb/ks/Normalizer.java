a6217a60365f8b2fcbe5e4b5ddad3c9440fe29fa
Andres Gomez Ferrer <andresgomezfrr@gmail.com>
2017-02-14 11:22:11 +0200

    Add metrics repoerts

--- src/main/java/rb/ks/Normalizer.java
+++ src/main/java/rb/ks/Normalizer.java
@@ -4,6 +4,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import rb.ks.builder.Builder;
 import rb.ks.builder.config.Config;
+import rb.ks.metrics.MetricsManager;
+
+import static rb.ks.builder.config.Config.ConfigProperties.METRIC_ENABLE;
 
 public class Normalizer {
     private static final Logger log = LoggerFactory.getLogger(Normalizer.class);
@@ -11,13 +14,23 @@ public class Normalizer {
     public static void main(String[] args) throws Exception {
         if (args.length == 1) {
             Config config = new Config(args[0]);
-            Builder builder = new Builder(config);
+            Builder builder = new Builder(config.clone());
 
-            Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-                builder.close();
-                log.info("Stopped Normalizer process.");
-            }));
+            if (config.getOrDefault(METRIC_ENABLE, false)) {
+                MetricsManager metricsManager = new MetricsManager(config.clone());
+                metricsManager.start();
 
+                Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+                    builder.close();
+                    metricsManager.interrupt();
+                    log.info("Stopped Normalizer process.");
+                }));
+            } else {
+                Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+                    builder.close();
+                    log.info("Stopped Normalizer process.");
+                }));
+            }
 
         } else {
             log.error("Execute: java -cp ${JAR_PATH} rb.ks.Normalizer <config_file>");
