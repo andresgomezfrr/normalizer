c3e5f371187b114e33742ee5f0ca58e47a25aabd
Andres Gomez Ferrer <andresgomezfrr@gmail.com>
2017-02-14 11:22:11 +0200

    Add match filter to do kStream.filter and kStream.notFilter

--- src/main/java/rb/ks/utils/Constants.java
+++ src/main/java/rb/ks/utils/Constants.java
@@ -4,4 +4,5 @@ public class Constants {
     public final static String __KEY = "__KEY";
     public final static String __STORES = "__STORES";
     public final static String __APP_ID = "__APP_ID";
+    public final static String __MATCH = "__MATCH";
 }
