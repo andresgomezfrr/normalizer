a5ffd28b203eb3c675e2d0319ebd50f3b79d26a0
Andres Gomez Ferrer <andresgomezfrr@gmail.com>
2017-02-14 11:22:11 +0200

    Add FieldFilter. Add Constants class

---
+++ src/main/java/rb/ks/utils/Constants.java
@@ -0,0 +1,7 @@
+package rb.ks.utils;
+
+public class Constants {
+    public final static String __KEY = "__KEY";
+    public final static String __STORES = "__STORES";
+    public final static String __APP_ID = "__APP_ID";
+}
