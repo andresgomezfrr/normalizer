18dcea730be684c96029359e6d1854aa9f09476e
Andrés Gómez <andresgomezfrr@gmail.com>
2017-02-14 11:22:11 +0200

    Improve docker
    * Improve docker
    
    * Fix log4j.xml to new log path

--- docker/build/normalizer-start.sh
+++ docker/build/normalizer-start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-envsubst < /alloc/var/normalizer/config/config_env.json > /alloc/var/normalizer/config/config.json
-/alloc/var/normalizer/bin/normalizer-start.sh /alloc/var/normalizer/config/config.json
+envsubst < /opt/normalizer/config/config_env.json > /opt/normalizer/config/config.json
+/opt/normalizer/bin/normalizer-start.sh /opt/normalizer/config/config.json
