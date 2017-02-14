96489abe65b28ff0b0e864465ce53eb5fdd8fdcc
Andres Gomez Ferrer <andresgomezfrr@gmail.com>
2017-02-14 11:22:11 +0200

    Fix filebootstraper

--- src/main/java/rb/ks/builder/bootstrap/FileBootstraper.java
+++ src/main/java/rb/ks/builder/bootstrap/FileBootstraper.java
@@ -3,6 +3,8 @@ package rb.ks.builder.bootstrap;
 import rb.ks.builder.Builder;
 import rb.ks.builder.config.Config;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.nio.file.Files;
 
 public class FileBootstraper extends ThreadBootstraper {
@@ -14,8 +16,18 @@ public class FileBootstraper extends ThreadBootstraper {
 
     @Override
     public void init(Builder builder, Config config) throws Exception {
-        String streamConfig = new String(Files.readAllBytes(config.get(FILE_PATH)));
-        builder.updateStreamConfig(streamConfig);
+        String filePath = config.get(FILE_PATH);
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(filePath));
+
+        StringBuilder stringBuffer = new StringBuilder();
+        String line;
+
+        while ((line = bufferedReader.readLine()) != null) {
+
+            stringBuffer.append(line).append("\n");
+        }
+
+        builder.updateStreamConfig(stringBuffer.toString());
     }
 
     @Override
