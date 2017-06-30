4e2aa254d62bbc391d4c26d0244cbc66ea75c1b9
Pablo Casares <lito92vforce@gmail.com>
2017-06-30 14:22:14 +0200

    MVP-445 Develop a normalizer function that allows to create new constant values
    * MVP-445 Develop a normalizer function that allows to create new constant values
    
    * MVP-445 Develop a normalizer function that allows to create new constant values
    
    * MVP-445 Develop a normalizer function that allows to create new constant values
    
    * MVP-445 Develop a normalizer function that allows to create new constant values

---
+++ service/src/main/java/io/wizzie/ks/normalizer/funcs/impl/FieldMapper.java
@@ -0,0 +1,61 @@
+package io.wizzie.ks.normalizer.funcs.impl;
+
+import io.wizzie.ks.normalizer.funcs.MapperFunction;
+import io.wizzie.ks.normalizer.metrics.MetricsManager;
+import org.apache.kafka.streams.KeyValue;
+
+import java.util.*;
+
+import static com.cookingfox.guava_preconditions.Preconditions.checkNotNull;
+
+
+public class FieldMapper extends MapperFunction {
+
+    public static final String VALUE = "value";
+    public static final String DIMENSION = "dimension";
+    public static final String DIMENSIONS = "dimensions";
+    public static final String OVERWRITE = "overwrite";
+
+    private final String ERROR_MESSAGE_PATTERN = "%s cannot be null";
+
+
+    List<Map<String, Object>> dimensionsToAdd;
+
+
+    @Override
+    public void prepare(Map<String, Object> properties, MetricsManager metricsManager) {
+
+        dimensionsToAdd = (List<Map<String, Object>>) properties.getOrDefault(DIMENSIONS, new ArrayList<>());
+
+        dimensionsToAdd.forEach(map -> {
+            checkNotNull(map.get(DIMENSION), String.format(ERROR_MESSAGE_PATTERN, DIMENSION));
+            checkNotNull(map.get(VALUE), String.format(ERROR_MESSAGE_PATTERN, VALUE));
+        });
+
+    }
+
+
+    @Override
+    public KeyValue<String, Map<String, Object>> process(String key, Map<String, Object> value) {
+
+        Map<String, Object> newEvent = new HashMap<>();
+        newEvent.putAll(value);
+
+        if (newEvent != null) {
+            dimensionsToAdd.stream()
+                    .forEach(m ->
+                    {
+                        if (newEvent.get(m.get(DIMENSION)) == null || (boolean) m.getOrDefault(OVERWRITE, false)) {
+                            newEvent.put((String) m.get(DIMENSION), m.get(VALUE));
+                        }
+                    });
+        }
+
+        return new KeyValue<>(key, newEvent);
+    }
+
+    @Override
+    public void stop() {
+
+    }
+}
---
+++ service/src/test/java/io/wizzie/ks/normalizer/funcs/impl/FieldMapperUnitTest.java
@@ -0,0 +1,173 @@
+
+
+package io.wizzie.ks.normalizer.funcs.impl;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.wizzie.ks.normalizer.builder.StreamBuilder;
+import io.wizzie.ks.normalizer.builder.config.Config;
+import io.wizzie.ks.normalizer.exceptions.PlanBuilderException;
+import io.wizzie.ks.normalizer.funcs.Function;
+import io.wizzie.ks.normalizer.funcs.MapperFunction;
+import io.wizzie.ks.normalizer.model.PlanModel;
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.StreamsConfig;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.Assert.*;
+
+public class FieldMapperUnitTest {
+
+    static Config config = new Config();
+
+    static {
+        config.put(StreamsConfig.APPLICATION_ID_CONFIG, "app-id-1");
+    }
+
+    private static StreamBuilder streamBuilder = new StreamBuilder(config, null);
+
+    @BeforeClass
+    public static void initTest() throws IOException, PlanBuilderException {
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        File file = new File(classLoader.getResource("field-mapper.json").getFile());
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        PlanModel model = objectMapper.readValue(file, PlanModel.class);
+        streamBuilder.builder(model);
+    }
+
+    @Test
+    public void building() {
+        Map<String, Function> functions = streamBuilder.getFunctions("stream1");
+        Function myFunc = functions.get("myFieldMapper");
+
+        assertNotNull(myFunc);
+        assertTrue(myFunc instanceof MapperFunction);
+        FieldMapper myFieldMapper = (FieldMapper) myFunc;
+
+        assertNotNull(myFieldMapper.dimensionsToAdd);
+
+        List<Map<String, Object>> dimensions = new LinkedList<>();
+
+        Map<String, Object> value = new HashMap<>();
+        value.put("dimension", "dimension1");
+        value.put("value", "defaultValue1");
+        value.put("overwrite", false);
+
+        dimensions.add(value);
+
+        value = new HashMap<>();
+        value.put("dimension", "dimension2");
+        value.put("value", "defaultValue2");
+        value.put("overwrite", true);
+
+        dimensions.add(value);
+
+        value = new HashMap<>();
+        value.put("dimension", "dimension3");
+        value.put("value", "defaultValue3");
+
+        dimensions.add(value);
+
+
+        assertEquals(dimensions, myFieldMapper.dimensionsToAdd);
+    }
+
+    @Test
+    public void processSimpleMessage() {
+        Map<String, Function> functions = streamBuilder.getFunctions("stream1");
+        Function myFieldFunc = functions.get("myFieldMapper");
+
+        assertNotNull(myFieldFunc);
+        assertTrue(myFieldFunc instanceof MapperFunction);
+        MapperFunction myReplaceMapper = (MapperFunction) myFieldFunc;
+
+        Map<String, Object> message1 = new HashMap<>();
+        message1.put("timestamp", 123456789L);
+        message1.put("dimension1", "value1");
+        message1.put("dimension2", "value2");
+        message1.put("dimension3", "value3");
+
+
+        Map<String, Object> expectedMessage1 = new HashMap<>();
+        expectedMessage1.put("timestamp", 123456789L);
+        expectedMessage1.put("dimension1", "value1");
+        expectedMessage1.put("dimension2", "defaultValue2");
+        expectedMessage1.put("dimension3", "value3");
+
+        KeyValue<String, Map<String, Object>> result1 = myReplaceMapper.process("KEY", message1);
+
+        assertEquals(new KeyValue<>("KEY", expectedMessage1), result1);
+
+    }
+
+    @Test
+    public void processNestedMessage() {
+        Map<String, Function> functions = streamBuilder.getFunctions("stream1");
+        Function myFieldFunc = functions.get("myFieldMapper");
+
+        assertNotNull(myFieldFunc);
+        assertTrue(myFieldFunc instanceof MapperFunction);
+        MapperFunction myReplaceMapper = (MapperFunction) myFieldFunc;
+
+        Map<String, Object> message1 = new HashMap<>();
+        message1.put("timestamp", 123456789L);
+        message1.put("dimension1", "{\"key1\":{\"key11\":\"value1\"}}");
+        message1.put("dimension2", "{\"key2\":{\"key22\":\"value2\"}}");
+        message1.put("dimension3", "value3");
+
+
+        Map<String, Object> expectedMessage1 = new HashMap<>();
+        expectedMessage1.put("timestamp", 123456789L);
+        expectedMessage1.put("dimension1", "{\"key1\":{\"key11\":\"value1\"}}");
+        expectedMessage1.put("dimension2", "defaultValue2");
+        expectedMessage1.put("dimension3", "value3");
+
+        KeyValue<String, Map<String, Object>> result1 = myReplaceMapper.process("KEY", message1);
+
+        assertEquals(new KeyValue<>("KEY", expectedMessage1), result1);
+
+    }
+
+    @Test
+    public void processNullDimension() {
+        Map<String, Function> functions = streamBuilder.getFunctions("stream1");
+        Function myFieldFunc = functions.get("myFieldMapper");
+
+        assertNotNull(myFieldFunc);
+        assertTrue(myFieldFunc instanceof MapperFunction);
+        MapperFunction myReplaceMapper = (MapperFunction) myFieldFunc;
+
+        Map<String, Object> message1 = new HashMap<>();
+        message1.put("timestamp", 123456789L);
+        message1.put("dimension1", "value1");
+        message1.put("dimension2", "value2");
+
+
+        Map<String, Object> expectedMessage1 = new HashMap<>();
+        expectedMessage1.put("timestamp", 123456789L);
+        expectedMessage1.put("dimension1", "value1");
+        expectedMessage1.put("dimension2", "defaultValue2");
+        expectedMessage1.put("dimension3", "defaultValue3");
+
+        KeyValue<String, Map<String, Object>> result1 = myReplaceMapper.process("KEY", message1);
+
+        assertEquals(new KeyValue<>("KEY", expectedMessage1), result1);
+    }
+
+    @AfterClass
+    public static void stop() {
+        streamBuilder.close();
+    }
+
+}
+
+
