9e99fdbb913ebd0227494bb83167362ff543d332
Pablo Casares <lito92vforce@gmail.com>
2017-07-20 13:02:06 +0200

    MVP-455 Create normalizer function to process timestamp field
    * MVP-455 Create normalizer function to process timestamp field
    
    * MVP-455 Create normalizer function to process timestamp field

---
+++ service/src/main/java/io/wizzie/ks/normalizer/exceptions/FunctionException.java
@@ -0,0 +1,7 @@
+package io.wizzie.ks.normalizer.exceptions;
+
+public class FunctionException extends RuntimeException {
+    public FunctionException(String message) {
+        super(message);
+    }
+}
