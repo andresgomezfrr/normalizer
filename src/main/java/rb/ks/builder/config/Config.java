6143f0e1ead94d640be05e8d4d3cff94bcf7930c
Juan Jesús Chorro Bergillo <jjchorrobe@gmail.com>
2018-02-14 11:22:11 +0200

    Added 'put' method and new constructors

--- src/main/java/rb/ks/builder/config/Config.java
+++ src/main/java/rb/ks/builder/config/Config.java
@@ -4,14 +4,31 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 
 public class Config {
     Map<String, Object> config;
 
+    public Config() {
+        config = new HashMap<>();
+    }
+
     public Config(String configPath) throws IOException {
+        init(configPath);
+    }
+
+    public Config(Map<String, Object> properties) {
+        init(properties);
+    }
+
+    private void init(String configPath) throws IOException {
         ObjectMapper objectMapper = new ObjectMapper();
-        config = objectMapper.readValue(new File(configPath), Map.class);
+        init(objectMapper.readValue(new File(configPath), Map.class));
+    }
+
+    private void init(Map<String, Object> properties) {
+        config = properties;
     }
 
     public <T> T get(String property) {
@@ -22,6 +39,13 @@ public class Config {
         }
 
         return ret;
+    }
+
+    public Config put(String property, Object value) {
+
+        config.put(property, value);
+
+        return this;
     }
 
     public static class ConfigProperties {
