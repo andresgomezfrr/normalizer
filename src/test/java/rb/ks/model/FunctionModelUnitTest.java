cc0139a07f6a37d7893cdb786c4126c1b3fe4424
Juan Jesús Chorro Bergillo <jjchorrobe@gmail.com>
2018-02-14 11:22:11 +0200

    Added 'FunctionModel' unit tests

---
+++ src/test/java/rb/ks/model/FunctionModelUnitTest.java
@@ -0,0 +1,39 @@
+package rb.ks.model;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static junit.framework.TestCase.*;
+
+
+public class FunctionModelUnitTest {
+
+    @Test
+    public void shouldWork() {
+
+        String className = "rb.ks.funcs.MyFunc";
+        String name = "myFunctionName";
+        Map<String, Object> properties = new HashMap<>();
+        List<String> stores = new ArrayList<>();
+
+        FunctionModel functionModelObject = new FunctionModel(name, className, properties, stores);
+
+        assertNotNull(functionModelObject.getClassName());
+        assertEquals(functionModelObject.getClassName(), className);
+
+        assertNotNull(functionModelObject.getName());
+        assertEquals(functionModelObject.getName(), name);
+
+        assertNotNull(functionModelObject.getProperties());
+        assertEquals(functionModelObject.getProperties(), properties);
+
+        assertNotNull(functionModelObject.getStores());
+        assertEquals(functionModelObject.getStores(), stores);
+
+    }
+
+}
