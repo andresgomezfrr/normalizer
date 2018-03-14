502caa71e5f2697f9f7290a3161ced113c733056
Juan Jesús Chorro Bergillo <jjchorrobe@gmail.com>
2018-03-14 11:22:11 +0200

    Added new file 'streamer-kafka-start.sh'

--- bin/streamer-kafka.sh
+++ bin/streamer-kafka.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+if [ $# -lt 1 ];
+then
+    echo "USAGE: $0 <bootstrap_kafka_server> <app_id> [stream_config_path]"
+    exit 1
+fi
+
 CURRENT=`pwd` && cd `dirname $0` && SOURCE=`pwd` && cd ${CURRENT} && PARENT=`dirname ${SOURCE}`
 
 for file in ${PARENT}/lib/*.jar;
@@ -11,6 +17,4 @@ if [ $# -eq 2 ]; then
   java -cp ${CLASSPATH} rb.ks.utils.bootstrap.StreamerKafkaConfig $1 $2
 elif [ $# -eq 3 ]; then
   java -cp ${CLASSPATH} rb.ks.utils.bootstrap.StreamerKafkaConfig $1 $2 $3
-else
-  echo -n 'streamer-kafka.sh <bootstrap_kafka_server> <app_id> [stream_config_path]'
 fi
