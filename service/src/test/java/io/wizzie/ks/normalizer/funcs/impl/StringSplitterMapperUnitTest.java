8aecaa71de0e90aea76206d9e3374b9f580e1142
Juan Chorro <jjchorrobe@gmail.com>
2018-03-14 11:22:11 +0200

    String splitter mapper
    * Add StringSplitterMapper function
    
    * Add StringSplitterMapper function unit tests and files

---
+++ service/src/test/java/io/wizzie/ks/normalizer/funcs/impl/StringSplitterMapperUnitTest.java
@@ -0,0 +1,106 @@
+package io.wizzie.ks.normalizer.funcs.impl;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.wizzie.ks.normalizer.builder.StreamBuilder;
+import io.wizzie.ks.normalizer.builder.config.Config;
+import io.wizzie.ks.normalizer.exceptions.PlanBuilderException;
+import io.wizzie.ks.normalizer.funcs.Function;
+import io.wizzie.ks.normalizer.model.PlanModel;
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.StreamsConfig;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+public class StringSplitterMapperUnitTest {
+
+    static Config config = new Config();
+
+    static {
+        config.put(StreamsConfig.APPLICATION_ID_CONFIG, "myApp");
+    }
+
+    private static StreamBuilder streamBuilder = new StreamBuilder(config, null);
+
+    @BeforeClass
+    public static void initTest() throws IOException, PlanBuilderException {
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        File file = new File(classLoader.getResource("string-splitter-mapper.json").getFile());
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        PlanModel model = objectMapper.readValue(file, PlanModel.class);
+        streamBuilder.builder(model);
+    }
+
+    @Test
+    public void building() {
+        Map<String, Function> functions = streamBuilder.getFunctions("myStream");
+        Function myFunc = functions.get("myStringSplitterFunction");
+
+        assertNotNull(myFunc);
+        assertTrue(myFunc instanceof StringSplitterMapper);
+        StringSplitterMapper myStringReplaceMapper = (StringSplitterMapper) myFunc;
+
+        assertEquals("DIM-H", myStringReplaceMapper.dimension);
+        assertEquals(">", myStringReplaceMapper.delimitier);
+        assertEquals(Arrays.asList("country", "province", "city"), myStringReplaceMapper.fieldNames);
+    }
+
+    @Test
+    public void processSimpleMessage(){
+        Map<String, Function> functions = streamBuilder.getFunctions("myStream");
+        Function myFunc = functions.get("myStringSplitterFunction");
+
+        assertNotNull(myFunc);
+        assertTrue(myFunc instanceof StringSplitterMapper);
+        StringSplitterMapper myStringReplaceMapper = (StringSplitterMapper) myFunc;
+
+        Map<String, Object> msg = new HashMap<>();
+        msg.put("DIM-A", "VALUE-A");
+        msg.put("DIM-B", 94);
+        msg.put("DIM-C", Arrays.asList("X", "Y", "Z"));
+        msg.put("DIM-H", "myCountry > myProvince > myCity");
+
+        Map<String, Object> expectedMsg = new HashMap<>();
+        expectedMsg.putAll(msg);
+        expectedMsg.put("country", "myCountry");
+        expectedMsg.put("province", "myProvince");
+        expectedMsg.put("city", "myCity");
+
+        assertEquals(new KeyValue<>("KEY", expectedMsg), myStringReplaceMapper.process("KEY", msg));
+    }
+
+    @Test
+    public void processNullValueMessage() {
+        Map<String, Function> functions = streamBuilder.getFunctions("myStream");
+        Function myFunc = functions.get("myStringSplitterFunction");
+
+        assertNotNull(myFunc);
+        assertTrue(myFunc instanceof StringSplitterMapper);
+        StringSplitterMapper myStringReplaceMapper = (StringSplitterMapper) myFunc;
+
+        assertEquals(new KeyValue<>("KEY", null), myStringReplaceMapper.process("KEY", null));
+    }
+
+    @Test
+    public void processNullKeyAndValueMessage() {
+        Map<String, Function> functions = streamBuilder.getFunctions("myStream");
+        Function myFunc = functions.get("myStringSplitterFunction");
+
+        assertNotNull(myFunc);
+        assertTrue(myFunc instanceof StringSplitterMapper);
+        StringSplitterMapper myStringReplaceMapper = (StringSplitterMapper) myFunc;
+
+        assertEquals(new KeyValue<>(null, null), myStringReplaceMapper.process(null, null));
+    }
+
+}
