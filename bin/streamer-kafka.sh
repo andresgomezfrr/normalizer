153b2d0bd857eb73115be987e7a8700feec9b177
Juan Jesús Chorro Bergillo <jjchorrobe@gmail.com>
2018-03-14 11:22:11 +0200

    Added relative path

--- bin/streamer-kafka.sh
+++ bin/streamer-kafka.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 
-for file in ${NORMALIZER_HOME}/lib/*.jar;
+CURRENT=`pwd` && cd `dirname $0` && SOURCE=`pwd` && cd ${CURRENT} && PARENT=`dirname ${SOURCE}`
+
+for file in ${PARENT}/lib/*.jar;
 do
     CLASSPATH="$CLASSPATH":"$file"
 done
 
 if [ $# -eq 2 ]; then
-  java -cp ${CLASSPATH} rb.ks.utils.StreamerKafkaConfig $0 $1
+  java -cp ${CLASSPATH} rb.ks.utils.bootstrap.StreamerKafkaConfig $1 $2
 elif [ $# -eq 3 ]; then
-  java -cp ${CLASSPATH} rb.ks.utils.StreamerKafkaConfig $0 $1 $2
+  java -cp ${CLASSPATH} rb.ks.utils.bootstrap.StreamerKafkaConfig $1 $2 $3
 else
   echo -n 'streamer-kafka.sh <bootstrap_kafka_server> <app_id> [stream_config_path]'
 fi
