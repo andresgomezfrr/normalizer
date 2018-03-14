b486fd7dd116a8b89e5f0777b73292a7547c8fba
Juan Jesús Chorro Bergillo <jjchorrobe@gmail.com>
2018-03-14 11:22:11 +0200

    Added ArrayFlattenMapper class, unit tests and JSON file

---
+++ src/main/java/rb/ks/funcs/impl/ArrayFlattenMapper.java
@@ -0,0 +1,61 @@
+package rb.ks.funcs.impl;
+
+import org.apache.kafka.streams.KeyValue;
+import rb.ks.funcs.FlatMapperFunction;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class ArrayFlattenMapper extends FlatMapperFunction {
+
+    String flatDimension;
+
+    @Override
+    public void prepare(Map<String, Object> properties) {
+        flatDimension = (String) properties.get("flat_dimension");
+    }
+
+    @Override
+    public Iterable<KeyValue<String, Map<String, Object>>> process(String key, Map<String, Object> value) {
+        List<KeyValue<String, Map<String, Object>>> results = Collections.singletonList(new KeyValue<>(key, value));
+
+        if (flatDimension != null) {
+
+            if (value.containsKey(flatDimension)) {
+                List<String> array = (List<String>) value.remove(flatDimension);
+
+                if(array != null) {
+                    results = array.stream().map(val -> {
+
+                        Map<String, Object> newValue = new HashMap<>();
+                        newValue.putAll(value);
+                        newValue.put(flatDimension, val);
+
+                        KeyValue<String, Map<String, Object>> kv = new KeyValue<>(key, newValue);
+
+                        return kv;
+
+                    }).collect(Collectors.toList());
+                }
+
+            }
+
+        }
+
+        return results;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append(" {")
+                .append("flat_dimension: ").append(flatDimension).append("} ");
+
+        return builder.toString();
+    }
+
+    @Override
+    public void stop() {
+
+    }
+}
