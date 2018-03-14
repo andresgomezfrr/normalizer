3c2a9d488db4537b44d8d3f09e7db9860675ae89
Juan Jesús Chorro Bergillo <jjchorrobe@gmail.com>
2018-03-14 11:22:11 +0200

    Added ConversionUtils class unit tests

---
+++ src/test/java/unit/ConversionUtilsUnitTest.java
@@ -0,0 +1,31 @@
+package unit;
+
+import org.junit.Test;
+import rb.ks.utils.ConversionUtils;
+
+import static org.junit.Assert.*;
+
+public class ConversionUtilsUnitTest {
+
+    @Test
+    public void conversionFromStringToLongObjectIsCorrectTest() {
+        String number = "1234567890";
+
+        assertEquals(ConversionUtils.toLong(number), new Long(1234567890L));
+    }
+
+    @Test
+    public void conversionFromIntegerToLongObjectIsCorrectTest() {
+        int number = 1234567890;
+
+        assertEquals(ConversionUtils.toLong(number), new Long(1234567890L));
+    }
+
+    @Test
+    public void conversionFromLongToLongObjectIsCorrectTest() {
+        long number = 1234567890L;
+
+        assertEquals(ConversionUtils.toLong(number), new Long(number));
+    }
+
+}
