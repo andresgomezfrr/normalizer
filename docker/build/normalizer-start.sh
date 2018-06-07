16db4c4ec6abc91a4373525831e721925923663b
Pablo Casares <lito92vforce@gmail.com>
2018-06-07 12:46:54 +0200

    WIZZIE-1917 Update normalizer repo to log4j2
    * WIZZIE-421 Generate installation normalizer-functions documentation
    
    * WIZZIE-1917 Update normalizer repo to log4j2
    
    * Delete DistanceStoreMapper

--- docker/build/normalizer-start.sh
+++ docker/build/normalizer-start.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 envsubst < /opt/normalizer/config/config_env.json > /opt/normalizer/config/config.json
+envsubst < /opt/normalizer/config/log4j2_env.xml > /opt/normalizer/config/log4j2_env.xml
 /opt/normalizer/bin/normalizer-start.sh /opt/normalizer/config/config.json
