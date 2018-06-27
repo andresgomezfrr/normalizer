a0b5e433422b1b7c47dea400a494b7dac218e578
Pablo Casares <lito92vforce@gmail.com>
2018-06-27 11:23:50 +0200

    WIZZIE-2051 Develop normalizer function for filter fields
    * WIZZIE-421 Generate installation normalizer-functions documentation
    
    * WIZZIE-1917 Update normalizer repo to log4j2
    
    * Deleted DistanceStoreMApper
    
    * WIZZIE-2051 Develop normalizer function for filter fields

--- functions/src/main/java/io/wizzie/ks/normalizer/funcs/impl/SimpleMapper.java
+++ functions/src/main/java/io/wizzie/ks/normalizer/funcs/impl/SimpleMapper.java
@@ -5,12 +5,15 @@ import io.wizzie.metrics.MetricsManager;
 import org.apache.kafka.streams.KeyValue;
 
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 public class SimpleMapper extends MapperFunction {
     List<MapperModel> mappers;
+    boolean deleteMode;
+    boolean deleteEmpty;
 
     @Override
     public void prepare(Map<String, Object> properties, MetricsManager metricsManager) {
@@ -18,31 +21,78 @@ public class SimpleMapper extends MapperFunction {
         mappers = maps.stream()
                 .map(map -> new MapperModel((List<String>) map.get("dimPath"), (String) map.get("as")))
                 .collect(Collectors.toList());
+        deleteMode = (boolean) properties.getOrDefault("deleteMode", false);
+        deleteEmpty = (boolean) properties.getOrDefault("deleteEmpty", true);
+
     }
 
     @Override
     public KeyValue<String, Map<String, Object>> process(String key, Map<String, Object> value) {
+
         Map<String, Object> newEvent = new HashMap<>();
 
         if (value != null && mappers != null) {
+
+            if (deleteMode) {
+                newEvent.putAll(value);
+            }
+
             mappers.forEach(mapper -> {
                 Integer depth = mapper.dimPath.size() - 1;
-
-                Map<String, Object> levelPath = new HashMap<>(value);
+                Map<String, Object> levelPath;
+                if (deleteMode) {
+                    levelPath = newEvent;
+                } else {
+                    levelPath = new HashMap<>(value);
+                }
+                List<Map<String, Object>> parents = new LinkedList<>();
+                parents.add(newEvent);
                 for (Integer level = 0; level < depth; level++) {
-                    if (levelPath != null) {
-                        levelPath = (Map<String, Object>) levelPath.get(mapper.dimPath.get(level));
+                    if (!deleteMode) {
+                        if (levelPath != null && levelPath.get(mapper.dimPath.get(level)) instanceof Map) {
+                            levelPath = (Map<String, Object>) levelPath.get(mapper.dimPath.get(level));
+                        } else {
+                            levelPath = null;
+                            break;
+                        }
+                    } else {
+                        if (levelPath.get(mapper.dimPath.get(level)) instanceof Map) {
+                            levelPath = (Map<String, Object>) levelPath.get(mapper.dimPath.get(level));
+                            parents.add(levelPath);
+                            if (level == depth - 1) {
+                                levelPath.remove(mapper.dimPath.get(depth));
+                            }
+                        } else {
+                            break;
+                        }
                     }
                 }
 
-                if (levelPath != null) {
+                if(depth == 0){
+                    newEvent.remove(mapper.dimPath.get(depth));
+                }
+                if (deleteEmpty && deleteMode && parents.size() > 1) {
+                    for (Integer level = depth - 1; level >= 0; level--) {
+                        if (((Map<String, Object>) parents.get(level).get(mapper.dimPath.get(level))).isEmpty()) {
+                            if (level == 0) {
+                                newEvent.remove(mapper.dimPath.get(level));
+                            } else {
+                                parents.get(level).remove(mapper.dimPath.get(level));
+                            }
+                        }
+                    }
+                }
+
+                if (levelPath != null && !deleteMode) {
                     Object newValue = levelPath.get(mapper.dimPath.get(depth));
-                    if (newValue != null) newEvent.put(mapper.as, newValue);
+                    newEvent.put(mapper.as, newValue);
                 }
             });
 
             return new KeyValue<>(key, newEvent);
-        } else {
+        } else
+
+        {
             return new KeyValue<>(key, null);
         }
     }
--- service/src/test/java/io/wizzie/ks/normalizer/funcs/impl/SimpleMapperUnitTest.java
+++ service/src/test/java/io/wizzie/ks/normalizer/funcs/impl/SimpleMapperUnitTest.java
@@ -1,8 +1,8 @@
 package io.wizzie.ks.normalizer.funcs.impl;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import io.wizzie.ks.normalizer.builder.StreamBuilder;
 import io.wizzie.bootstrapper.builder.Config;
+import io.wizzie.ks.normalizer.builder.StreamBuilder;
 import io.wizzie.ks.normalizer.exceptions.PlanBuilderException;
 import io.wizzie.ks.normalizer.funcs.Function;
 import io.wizzie.ks.normalizer.model.PlanModel;
@@ -28,6 +28,9 @@ public class SimpleMapperUnitTest {
     }
 
     private static StreamBuilder streamBuilder = new StreamBuilder(config, null);
+    private static StreamBuilder streamBuilder2 = new StreamBuilder(config, null);
+    private static StreamBuilder streamBuilder3 = new StreamBuilder(config, null);
+    private static StreamBuilder streamBuilder4 = new StreamBuilder(config, null);
 
     @BeforeClass
     public static void initTest() throws IOException, PlanBuilderException {
@@ -37,6 +40,18 @@ public class SimpleMapperUnitTest {
         ObjectMapper objectMapper = new ObjectMapper();
         PlanModel model = objectMapper.readValue(file, PlanModel.class);
         streamBuilder.builder(model);
+
+        File file2 = new File(classLoader.getResource("simple-mapper-delete.json").getFile());
+        PlanModel model2 = objectMapper.readValue(file2, PlanModel.class);
+        streamBuilder2.builder(model2);
+
+        File file3 = new File(classLoader.getResource("simple-mapper-delete-with-empty.json").getFile());
+        PlanModel model3 = objectMapper.readValue(file3, PlanModel.class);
+        streamBuilder3.builder(model3);
+
+        File file4 = new File(classLoader.getResource("simple-mapper-delete-two-fields.json").getFile());
+        PlanModel model4 = objectMapper.readValue(file4, PlanModel.class);
+        streamBuilder4.builder(model4);
     }
 
     @Test
@@ -95,6 +110,185 @@ public class SimpleMapperUnitTest {
         assertEquals("TEST-Z", value.get("Q"));
         assertEquals("TEST-P", value.get("P"));
         assertEquals(123456789, value.get("timestamp"));
+    }
+
+    @Test
+    public void processSimpleMessageShouldWork() {
+        Map<String, Function> functions = streamBuilder.getFunctions("stream1");
+        Function myFunc = functions.get("myMapper");
+
+        assertNotNull(myFunc);
+        assertTrue(myFunc instanceof SimpleMapper);
+        SimpleMapper myMapper = (SimpleMapper) myFunc;
+
+        Map<String, Object> message = new HashMap<>();
+        message.put("timestamp", 123456789);
+
+        message.put("A", "shouldWork");
+
+
+        Map<String, Object> y = new HashMap<>();
+        Map<String, Object> w = new HashMap<>();
+        w.put("Z", "TEST-Z");
+        w.put("P", "TEST-P");
+        y.put("W", w);
+        message.put("Y", y);
+
+        KeyValue<String, Map<String, Object>> mapMessage = myMapper.process("key1", message);
+        assertEquals("key1", mapMessage.key);
+
+        Map<String, Object> value = mapMessage.value;
+        assertEquals("TEST-Z", value.get("Q"));
+        assertEquals("TEST-P", value.get("P"));
+        assertEquals(123456789, value.get("timestamp"));
+        assertEquals(3, value.keySet().size());
+    }
+
+    @Test
+    public void processSimpleMessageDeleteMode() {
+        Map<String, Function> functions = streamBuilder2.getFunctions("stream1");
+        Function myFunc = functions.get("myMapper");
+
+        assertNotNull(myFunc);
+        assertTrue(myFunc instanceof SimpleMapper);
+        SimpleMapper myMapper = (SimpleMapper) myFunc;
+
+        Map<String, Object> message = new HashMap<>();
+        message.put("timestamp", 123456789);
+
+        Map<String, Object> a = new HashMap<>();
+        Map<String, Object> b = new HashMap<>();
+        b.put("C", "TEST-C");
+        a.put("B", b);
+        message.put("A", a);
+
+
+        Map<String, Object> y = new HashMap<>();
+        Map<String, Object> w = new HashMap<>();
+        w.put("Z", "TEST-Z");
+        w.put("P", "TEST-P");
+        y.put("W", w);
+        message.put("Y", y);
+
+        KeyValue<String, Map<String, Object>> mapMessage = myMapper.process("key1", message);
+        assertEquals("key1", mapMessage.key);
+        Map<String, Object> value = mapMessage.value;
+
+        Map<String, Object> messageExpected = new HashMap<>();
+        Map<String, Object> yExpected = new HashMap<>();
+        Map<String, Object> wExpected = new HashMap<>();
+        wExpected.put("P", "TEST-P");
+        yExpected.put("W", wExpected);
+        assertEquals(yExpected, value.get("Y"));
+        assertEquals(123456789, value.get("timestamp"));
+        assertEquals(2, value.keySet().size());
+    }
+
+    @Test
+    public void processSimpleMessageDeleteModeTwoFields() {
+        Map<String, Function> functions = streamBuilder4.getFunctions("stream1");
+        Function myFunc = functions.get("myMapper");
+
+        assertNotNull(myFunc);
+        assertTrue(myFunc instanceof SimpleMapper);
+        SimpleMapper myMapper = (SimpleMapper) myFunc;
+
+        Map<String, Object> message = new HashMap<>();
+        message.put("timestamp", 123456789);
+        message.put("value",2);
+        message.put("other", "value");
+
+        KeyValue<String, Map<String, Object>> mapMessage = myMapper.process("key1", message);
+        assertEquals("key1", mapMessage.key);
+        Map<String, Object> value = mapMessage.value;
+
+        Map<String, Object> messageExpected = new HashMap<>();
+        messageExpected.put("other", "value");
+
+        assertEquals(messageExpected, value);
+        assertEquals(1, value.keySet().size());
+    }
+
+
+    @Test
+    public void processSimpleMessageDeleteModeShouldWork() {
+        Map<String, Function> functions = streamBuilder2.getFunctions("stream1");
+        Function myFunc = functions.get("myMapper");
+
+        assertNotNull(myFunc);
+        assertTrue(myFunc instanceof SimpleMapper);
+        SimpleMapper myMapper = (SimpleMapper) myFunc;
+
+        Map<String, Object> message = new HashMap<>();
+        message.put("timestamp", 123456789);
+
+
+        message.put("A", "shouldWork");
+
+
+        Map<String, Object> y = new HashMap<>();
+        Map<String, Object> w = new HashMap<>();
+        w.put("Z", "TEST-Z");
+        w.put("P", "TEST-P");
+        y.put("W", w);
+        message.put("Y", y);
+
+        KeyValue<String, Map<String, Object>> mapMessage = myMapper.process("key1", message);
+        assertEquals("key1", mapMessage.key);
+        Map<String, Object> value = mapMessage.value;
+
+        Map<String, Object> messageExpected = new HashMap<>();
+        Map<String, Object> yExpected = new HashMap<>();
+        Map<String, Object> wExpected = new HashMap<>();
+        wExpected.put("P", "TEST-P");
+        yExpected.put("W", wExpected);
+        assertEquals(yExpected, value.get("Y"));
+        assertEquals(123456789, value.get("timestamp"));
+        assertEquals("shouldWork", value.get("A"));
+        assertEquals(3, value.keySet().size());
+    }
+
+    @Test
+    public void processSimpleMessageDeleteWithoutEmptyDeleteMode() {
+        Map<String, Function> functions = streamBuilder3.getFunctions("stream1");
+        Function myFunc = functions.get("myMapper");
+
+        assertNotNull(myFunc);
+        assertTrue(myFunc instanceof SimpleMapper);
+        SimpleMapper myMapper = (SimpleMapper) myFunc;
+
+        Map<String, Object> message = new HashMap<>();
+        message.put("timestamp", 123456789);
+
+        Map<String, Object> a = new HashMap<>();
+        Map<String, Object> b = new HashMap<>();
+        b.put("C", "TEST-C");
+        a.put("B", b);
+        message.put("A", a);
+
+
+        Map<String, Object> y = new HashMap<>();
+        Map<String, Object> w = new HashMap<>();
+        w.put("Z", "TEST-Z");
+        w.put("P", "TEST-P");
+        y.put("W", w);
+        message.put("Y", y);
+
+        KeyValue<String, Map<String, Object>> mapMessage = myMapper.process("key1", message);
+        assertEquals("key1", mapMessage.key);
+        Map<String, Object> value = mapMessage.value;
+
+        Map<String, Object> yExpected = new HashMap<>();
+        Map<String, Object> wExpected = new HashMap<>();
+        wExpected.put("P", "TEST-P");
+        yExpected.put("W", wExpected);
+        assertEquals(yExpected, value.get("Y"));
+
+        Map<String, Object> bExpected = new HashMap<>();
+        bExpected.put("B", new HashMap<>());
+        assertEquals(bExpected, value.get("A"));
+        assertEquals(123456789, value.get("timestamp"));
+        assertEquals(3, value.keySet().size());
     }
 
     @Test
