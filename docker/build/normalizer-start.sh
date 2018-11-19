c731496f585ae331b2b0cd10e85df0f7858c9f06
Pablo Casares <lito92vforce@gmail.com>
2018-11-19 17:45:03 +0100

    Fix normalizer-start.sh output log4j2.xml file (#30)

--- docker/build/normalizer-start.sh
+++ docker/build/normalizer-start.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 envsubst < /opt/normalizer/config/config_env.json > /opt/normalizer/config/config.json
-envsubst < /opt/normalizer/config/log4j2_env.xml > /opt/normalizer/config/log4j2_env.xml
+envsubst < /opt/normalizer/config/log4j2_env.xml > /opt/normalizer/config/log4j2.xml
 
 exec /opt/normalizer/bin/normalizer-start.sh /opt/normalizer/config/config.json
